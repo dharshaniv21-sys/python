7b7393006e5903d0aa4a212f604f049fe97cb761
dharshaniv21-sys <dharshaniv21@gmail.com>
2026-09-03 13:02:14 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,66 @@
+print("Hello, World!")
+#assignment operators
+x= 10
+x+= 5
+print(x)
+x-=2
+print(x)
+x*=3
+print(x)
+
+#bank balance
+balance= 10000
+
+deposit= 5000
+balance += deposit
+
+print("After Deposit:", balance)
+
+
+withdraw= 2000
+balance-= withdraw
+
+print("After withdraw:", balance)
+
+#comparsion operator
+a= 10
+b= 20
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+
+
+#age eligibility checker
+age= int(input("Enter your age: "))
+
+print("Eligible to vote:", age>=18)
+
+
+#pass or fail checker
+marks= int(input("Enter your marks:"))
+
+print("passed:", marks>=40)
+
+#login validation
+correct_username="admin"
+correct_password="password123"
+
+username= input("Enter your username: ")
+password= input("Enter your password: ")
+
+
+
+
+
+
+
+
+
+
+
+
