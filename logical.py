cafb511c6d929add9be9a1b57b7d08f394e103cf
dharshaniv21-sys <dharshaniv21@gmail.com>
2026-09-05 13:05:23 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,24 @@
+#logical operatos
+age = 25
+citizen = True
+print(age >= 18 and citizen==True)
+      
+
+
+age = 16
+citizen = True
+print(age >=18 and citizen==True)
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
